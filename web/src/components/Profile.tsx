00ed39525cc0d84cf2949992336c04ba71b25c0d
João Pedro Moreno <devmoreno2003@gmail.com>
2023-05-21 19:36:05 -0300

    Route layout defined, logout route created, new memory page and authentication middleware

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -17,7 +17,7 @@ export function Profile() {
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
         <a
-          href=""
+          href="/api/auth/logout"
           className="block text-red-500 transition-colors hover:text-red-400"
         >
           Desconectar
